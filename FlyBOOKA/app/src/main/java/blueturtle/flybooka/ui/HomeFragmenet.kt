92063c12f9ed2f35f55ba811936387dfa32563bc
Erica <yyjin1217@naver.com>
2022-04-29 16:49:27 +0900

    라이브러리 추가

---
+++ FlyBOOKA/app/src/main/java/blueturtle/flybooka/ui/HomeFragmenet.kt
@@ -0,0 +1,18 @@
+package blueturtle.flybooka.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import blueturtle.flybooka.R
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        
+        return view
+    }
+
+}
