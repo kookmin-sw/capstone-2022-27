12d27e0565f1069694e61ca7beed6b6bdc4ed2eb
CodingNoye <codingnoye@kookmin.ac.kr>
2022-05-07 19:18:20 +0900

    백엔드 1차 완료

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('book/detail/<int:id>', book.detail),
     path('book/search_keyword/<keywords>/<int:page>', book.search_keyword),
     path('book/search/<keyword>/<int:page>', book.search),
+    path('book/firstpage/', book.firstpage),
+    path('book/firstpage_list/', book.firstpage_list),
+    path('book/mainpage/', book.mainpage),
 ]
