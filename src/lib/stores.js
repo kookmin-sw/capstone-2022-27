4373d46814fc3b307ff66399cfd9ffded6fddad4
Erica <yyjin1217@naver.com>
2022-05-13 17:27:09 +0900

    token과 함께 닉네임 저장

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -2,10 +2,15 @@ import { browser } from '$app/env';
 import { writable } from 'svelte/store'
 // import { browser } from "$app/env";
 
-export const TOKEN = writable(browser && localStorage.getItem("token"))
+export const stores_TOKEN = writable(browser && localStorage.getItem("token"))
+export const stores_nickname = writable(browser && localStorage.getItem("nickname"))
 
 if(browser){
-    TOKEN.subscribe(value => {
+    stores_TOKEN.subscribe(value => {
         localStorage.setItem("token", value);
+    });
+
+    stores_nickname.subscribe(value => {
+        localStorage.setItem("nickname", value);
     });
 }
